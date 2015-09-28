c3e443e02d22bb76d587b5dde9342ca01bed425c
Kelly Leppik <kelllep@greeny.cs.tlu.ee>
2015-09-28 15:30:04 +0300

    andmed andmebaasi sisestatud

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
     
     */
     require_once("../config_global.php");
-    $database = "if15_romil_1";
+    $database = "kelllep";
 	
 	//paneme sessiooni serveris tööle, saaame kasutada SESSION[]
     session_start();
@@ -24,6 +24,9 @@
         $stmt->bind_result($id_from_db, $email_from_db);
         $stmt->execute();
         if($stmt->fetch()){
+			//saime andmed kätte
+			
+			
 			echo "Kasutaja logis sisse id=".$id_from_db;
 			
 			//sessioon, salvestatakse serveros
@@ -52,5 +55,35 @@
 		
 		$mysqli->close();	
 	}
+	
+	function createCarPlate($plate, $car_color) {
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $stmt = $mysqli->prepare("INSERT INTO car_plates (user_id, number_plate, color) VALUES (?,?,?)");
+        // i - on user_id INT
+        $stmt->bind_param("iss", $_SESSION['logged_in_user_id'], $plate, $car_color);
+        
+		
+		$message = "";
+		if ($stmt->execute()){
+			//õnnestus
+			$message = "edukalt andmebaasi salvestatud!";
+			
+		}
+		$stmt->close();
+        
+        $mysqli->close();
+		
+		//saadan sõnumi tagasi
+		return $message;
+	}
+	
+	function getAllData(){
+		
+		$mysqli = new mysqli($GLOBALS["servername"])
+		
+		$stmt = $mysqli->prepare("SELECT id, user_id, number")
+		$stmt->bind_param($id_from_db, $user_id_from_db, $number)
+	}
+	
 	
 ?>
