0d81b02807073b1c5309a255d12dee38a24622b9
Kelly Leppik <kelllep@greeny.cs.tlu.ee>
2015-09-28 13:17:09 +0300

    functions.pfp and longin

--- functions.php
+++ functions.php
@@ -1,16 +1,38 @@
 <?php
-    //functions.php
-    //siia tulevad funktsioonid
-    function hello1 ($name, $age){
-		echo "Tere ".$name. "kes on ".$age." aastat vana";
-	}
-    
-	function hello2 ($name){
-		return "Tere ".$name;
+    //loome AB ühenduse
+    require_once("../config.php");
+    $database = "if15_romil_1";
+   
+	function logInUser($email, $hash){
+		
+		//GLOBALS saab kätte kõik muutujad mis kasutusel		
+		//muutuja väljaspool funktsiooni
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+        $stmt->bind_param("ss", $email, $hash);
+        $stmt->bind_result($id_from_db, $email_from_db);
+        $stmt->execute();
+        if($stmt->fetch()){
+			echo "Kasutaja logis sisse id=".$id_from_db;
+        }else{
+            echo "Wrong credentials!";
+        }
+        $stmt->close();
+		$mysqli->close();
+		
 	}
 	
-	echo "<br>";
-	hello1("Juku", 5);
-	echo "<br>";
-	echo hello2("Juhan");
+	function createUser($create_email, $hash){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?,?)");
+        $stmt->bind_param("ss", $create_email, $hash);
+        $stmt->execute();
+        $stmt->close();
+		
+		$mysqli->close();	
+	}
+	
 ?>
